86b8a5bf22c2a86c5086665b2dd9f1dfa59458ee
André Leblanc <a.leblanc@live.com>
2018-05-29 20:50:59 -0400

    initial commit must change connection string

---
+++ ReproSequence/DietResult.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using LanBO.ServiceModel.Interface.Tables;
+using LanBO.ServiceModel.Interface.Tables.REIBeef;
+using ServiceStack.DataAnnotations;
+using ServiceStack.OrmLite;
+
+namespace LanBO.ServiceModel.Tables.REIBeef
+{
+	[Schema("REIBeef")] //Specifique to REIBeef
+	public class DietResult
+	{
+		[Sequence("DietId"), ReturnOnInsert]
+		public int DietId { get; set; }
+		public int AccessFiber { get; set; }
+		public int MainFiber { get; set; }
+		public int FiberQuality { get; set; }
+		public int FiberRegularity { get; set; }
+		public bool ValidCategory { get; set; }
+		public ulong RowVersion { get; set; }
+
+		[Default(OrmLiteVariables.SystemUtc)]
+		public DateTime? CreatedDateUtc { get; set; }
+
+		[Default(OrmLiteVariables.SystemUtc)]
+		public DateTime? UpdatedDateUtc { get; set; }
+
+		[PrimaryKey, AutoId, ReturnOnInsert]
+		public Guid? RowId { get; set; }
+
+	}
+}
